21b8e62806f7061a95a275e9173d9e02418377cd
Tunahan <47463551+devtunahan@users.noreply.github.com>
2024-02-25 17:14:52 +0100

    adjusted script

--- notion_prayer.py
+++ notion_prayer.py
@@ -3,17 +3,13 @@
 import requests
 from notion_client import Client
 from datetime import datetime, timedelta
-from dotenv import load_dotenv
-import os
-
-# Load environment variables
-load_dotenv()
+import pytz  # To handle timezone conversions
 
 # Your Notion integration token and database ID
-NOTION_TOKEN = os.getenv('NOTION_TOKEN')
-NOTION_DATABASE_ID = os.getenv('NOTION_DATABASE_ID')
-CITY = os.getenv('CITY')
-COUNTRY = os.getenv('COUNTRY')
+NOTION_TOKEN = 'secret_kVDxyBImuxq1RI6LU3D1xFew3dRJT2XJDMzJM7Noprw'
+NOTION_DATABASE_ID = 'a838e3e0f4fa4e368cc7f1b109751b28'
+CITY = 'Wels'
+COUNTRY = 'Austria'
 
 # Define the prayer times we're interested in
 PRAYER_TIMES_OF_INTEREST = ['Imsak', 'Fajr', 'Dhuhr', 'Asr', 'Maghrib', 'Isha']
@@ -22,8 +18,8 @@ def get_prayer_times():
     url = f"http://api.aladhan.com/v1/timingsByCity?city={CITY}&country={COUNTRY}&method=13"
     response = requests.get(url)
     data = response.json()['data']['timings']
-
-    # Map the API response to specified names and order
+    
+    # Map the API response to your specified names and order
     prayer_times = {
         'Imsak': data['Fajr'],
         'Fajr': data['Sunrise'],
@@ -32,27 +28,30 @@ def get_prayer_times():
         'Maghrib': data['Maghrib'],
         'Isha': data['Isha']
     }
-
+    
     # Return the prayer times in the specified order
     return {time: prayer_times[time] for time in PRAYER_TIMES_OF_INTEREST}
 
 def update_notion(prayer_times):
     notion = Client(auth=NOTION_TOKEN)
-    today_date = datetime.now().strftime('%Y-%m-%d')
+    today_date = datetime.now(pytz.timezone("Europe/Vienna"))
+    formatted_date = today_date.strftime('%Y-%m-%d')
 
     for prayer_name in PRAYER_TIMES_OF_INTEREST:
         time = prayer_times[prayer_name]
-        prayer_time = datetime.strptime(f"{today_date} {time}", '%Y-%m-%d %H:%M')
-
+        # Convert prayer time to your local timezone and format it
+        prayer_time = datetime.strptime(f"{formatted_date} {time}", '%Y-%m-%d %H:%M')
+        local_prayer_time = prayer_time.astimezone(pytz.timezone("Europe/Vienna"))
+        
         if prayer_name == 'Fajr':
             # For Fajr, set the start time 20 minutes earlier
-            start_time = prayer_time - timedelta(minutes=20)
-            end_time = prayer_time
+            start_time = local_prayer_time - timedelta(minutes=20)
+            end_time = local_prayer_time
         else:
             # For other prayers, set a duration of 10 minutes
-            start_time = prayer_time
-            end_time = prayer_time + timedelta(minutes=10)
-
+            start_time = local_prayer_time
+            end_time = local_prayer_time + timedelta(minutes=10)
+        
         # Create a new entry for each prayer time
         new_entry = {
             "parent": {"database_id": NOTION_DATABASE_ID},
